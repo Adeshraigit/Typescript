a6139aabc72c3396e50b512802d3771a6e94de3e
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-02-09 23:15:00 +0530

    learned inteface and types

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// let x: number = 1;
+// console.log(x);
+// function greet(firstname: string){
+//     console.log("Hello " + firstname);      
+// }
+// greet("Adesh");
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+// let ans: number = sum(1, 2);
+// console.log(ans);            
+// function delayedCall(fn: () => void) {
+//     setTimeout(fn, 1000);       
+// }
+// delayedCall(function() {
+//     console.log("Hello");
+// }); 
+let greet = () => {
+    console.log("Hello");
+};
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+// let x: number = 1;
+
+// console.log(x);
+
+// function greet(firstname: string){
+//     console.log("Hello " + firstname);      
+// }
+
+// greet("Adesh");
+
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+
+// let ans: number = sum(1, 2);
+
+// console.log(ans);            
+
+// function delayedCall(fn: () => void) {
+//     setTimeout(fn, 1000);       
+// }
+
+// delayedCall(function() {
+//     console.log("Hello");
+// }); 
+
+// let greet = () => {
+//     console.log("Hello");               
+// }
+
+// Interfaces
+// function greet(user : {name: string, age: number}){
+//     console.log("Hello " + user.name);  
+//     console.log("Your age is " + user.age);       
+// }
+
+// greet({               
+//     name: "Adesh",              
+//     age: 21            
+// });
+
+// let firstname: string = "Adesh";    
+// let age: number = 21;       
+
+// interface UserType {        
+//     name: string;        
+//     age: number;        
+// }     
+        
+// function greet(user: UserType) {        
+//     console.log("Hello " + user.name);        
+//     console.log("Your age is " + user.age);        
+// }       
+        
+// greet({        
+//     name: "Adesh",        
+//     age: 21        
+// });
+
